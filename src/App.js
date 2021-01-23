0d3ec47857a087f8eedaf664d6f3d37efe7d44e9
Zachary Kramer <zckramer83@gmail.com>
2021-01-23 16:47:51 -0500

    fixed the package.json and added react-jss to work on mobile

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { useState } from 'react';
 
 import Header from './components/Header';
 import About from './components/About';
-// import Menu from './components/Menu';
-// import MenuFile from './Tanks_Menu_5.pdf';
 import Menu from './components/MenuSections';
-// import MenuFile from './images/meat'
 
 import MeatImg1 from './images/meat01.jpg';
 
@@ -29,7 +26,6 @@ function App() {
     <div className="App"  style={{ backgroundImage: `url(${MeatImg1})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'} }>
       <Header navClick={(e)=>handleNavClick(e)} />
       <div className="Body">
-        {/* {isShowMenu ? <Menu menu={MenuFile} /> : <About  />} */}
         {isShowMenu ? <Menu /> : <About  />}
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,10 @@
-import pigImage from '../images/pigline.png';
 import TankLogo from '../images/Logo_Tank.png';
 
 const Header = (props) => {
     return (
         <div className="Header" >
-            {/* <img  id="Logo__Header" src={pigImage} } /> */}
             <img className="Header__Item" src={TankLogo} alt="Tank Logo" id="Tank-Logo" onClick={()=>props.navClick("PIG")}/>
             <div className="Header__Item" onClick={()=>props.navClick("MENU")} >Menu</div>
-            
-            {/* <div className="Header__Item">Catering</div>
-            <div className="Header__Item">Contact</div> */}
         </div>
     )
 }
