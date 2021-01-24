80a1803a647b2acd611df37c478c0c49efd5126c
Zachary Kramer <zckramer83@gmail.com>
2021-01-23 20:59:20 -0500

    added react-jss functionality and state. next style components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,95 @@
 import './App.css';
 import { useState } from 'react';
+import { createUseStyles } from 'react-jss';
+import { useMediaQuery } from 'react-responsive';
 
 import Header from './components/Header';
 import About from './components/About';
-import Menu from './components/MenuSections';
 
 import MeatImg1 from './images/meat01.jpg';
+import MenuSections from './components/MenuSections';
+
+const useDesktopStyles = createUseStyles({
+    app: {
+        color: '#FFFFF7',
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${MeatImg1})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    },
+    body: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        color: 'black',
+        position: 'absolute',
+        top: '0',
+        width: '100%',
+        backgroundColor: '#D7DEE6',
+        maxHeight: '80px',
+        minHeight: '40px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        fontSize: 'calc(10px + 2vmin)',
+        marginBottom: '5rem'
+    }
+});
+
+const useMobileStyles = createUseStyles({
+    app: {
+        color: '#FFFFF7',
+        width: '100vw',
+        height: '100vh',      
+        backgroundImage: `url(${MeatImg1})`, 
+        backgroundSize: 'fill', 
+        backgroundRepeat: 'no-repeat',
+    },
+    body: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        color: 'black',
+        position: 'absolute',
+        top: '0',
+        width: '100%',
+        backgroundColor: '#D7DEE6',
+        maxHeight: '80px',
+        minHeight: '40px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        fontSize: 'calc(10px + 2vmin)',
+        marginBottom: '5rem'
+    }
+});
 
 function App() {
+  const isMobileView = useMediaQuery({ query: `(max-width: 760px)` });
   const [isShowMenu, setIsShowMenu] = useState(false);
+  const mobileClasses = useMobileStyles();
+  const desktopClasses = useDesktopStyles();
 
   function handleNavClick (target) {
     if (target === "MENU") {
       console.log(target);
-      setIsShowMenu(true)
+      setIsShowMenu(true);
     } else setIsShowMenu(false);
-
-    if (target === "PIG") {
-      console.log(target);
-      setIsShowMenu(false);
-    }
   }
 
   return (
-    <div className="App"  style={{ backgroundImage: `url(${MeatImg1})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'} }>
-      <Header navClick={(e)=>handleNavClick(e)} />
-      <div className="Body">
-        {isShowMenu ? <Menu /> : <About  />}
-      </div>
+    <div className={isMobileView ? mobileClasses.app : desktopClasses.app} >
+        <div className={isMobileView ? mobileClasses.body : desktopClasses.body}>
+            <Header className={isMobileView ? mobileClasses.header : desktopClasses.header} navClick={(e)=>handleNavClick(e)}/>
+            {isShowMenu ? <MenuSections /> : <About />}
+        </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import TankLogo from '../images/Logo_Tank.png';
 const Header = (props) => {
     return (
         <div className="Header" >
-            <img className="Header__Item" src={TankLogo} alt="Tank Logo" id="Tank-Logo" onClick={()=>props.navClick("PIG")}/>
+            <img className="Header__Item" src={TankLogo} alt="Tank Logo" id="Tank-Logo" onClick={()=>props.navClick("ABOUT")}/>
             <div className="Header__Item" onClick={()=>props.navClick("MENU")} >Menu</div>
         </div>
     )
