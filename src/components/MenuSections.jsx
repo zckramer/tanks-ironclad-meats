51dfe4b6c1c31282d9de72b83c193df8667ef162
Zachary Kramer <zckramer83@gmail.com>
2021-01-28 16:52:21 -0500

    finished desktop resolution layout

--- src/components/MenuSections.jsx
+++ src/components/MenuSections.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 
-const stylesDesktop = createUseStyles({
+const useStyles = createUseStyles({
     menu: {
         display: 'flex',
         flexDirection: 'row-reverse',
+        justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
-        marginTop: 100,
+        height: '100%',
     },
     menuButtonContainer: {
         display: 'flex',
@@ -23,22 +23,32 @@ const stylesDesktop = createUseStyles({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'whitesmoke',
-        border: '3px solid whitesmoke',
+        border: '1px solid black',
         borderRadius: '8px',
         userSelect: 'none',
         cursor: 'pointer',
-        margin: '2px',
-        width: '3rem',
-        height: '20%'
+        margin: 2,
+        padding: 4,
+        width: '30%',
+        height: '10%',
+        transform: 'rotate(-90deg)',
+        '&:hover': {
+            transform: 'rotate(-90deg) scale(1.2)',
+        },
+    },
+    theMenu: {
+        margin: '1rem',
+        height: 800,
+        width: 800
     }
-
 })
 
-const MenuSections = (props) => {
+const MenuSections = () => {
+    const styles = useStyles();
     const [sections, setSections] = useState([
-        'http://placekitten.com/1200/1200',
-        'http://placekitten.com/1000/1000',
-        'http://placekitten.com/600/600'
+        'http://placekitten.com/800/800',
+        'http://placekitten.com/800/800',
+        'http://placekitten.com/800/800'
     ])
 
     const [sectionToShow, setSectionToShow] = useState(sections[0]);
@@ -48,12 +58,12 @@ const MenuSections = (props) => {
     }
 
     return (
-        <div className={stylesDesktop.menu}>
-            <img src={sectionToShow} style={{height: "80%", width: "80%", borderRadius:"16px"}}/>
-            <div className={stylesDesktop.menuButtonContainer}>
-                <div className={stylesDesktop.menuButton} onClick={()=>handleChangePage(0)} style={{width: '20%'}}>A</div>
-                <div className={stylesDesktop.menuButton} onClick={()=>handleChangePage(1)} style={{width: '20%'}}>B</div>
-                <div className={stylesDesktop.menuButton} onClick={()=>handleChangePage(2)} style={{width: '20%'}}>C</div>
+        <div className={styles.menu}>
+            <img className={styles.theMenu} src={sectionToShow} style={{height: "80%", width: "80%", borderRadius:"16px"}}/>
+            <div className={styles.menuButtonContainer}>
+                <div className={styles.menuButton} onClick={()=>handleChangePage(0)} >Platters</div>
+                <div className={styles.menuButton} onClick={()=>handleChangePage(1)} >A La Carte</div>
+                <div className={styles.menuButton} onClick={()=>handleChangePage(2)} >Sides and Sauces</div>
             </div>
         </div>
     )
