88996fe5ece9ed0d1226e9c1402a917e4015bc2e
Zack Kramer <zckramer83@gmail.com>
2021-01-30 13:34:40 -0500

    finished menu portrait and landscape styling. add anchors to about locations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import './App.css';
 import { useState } from 'react';
 import { createUseStyles } from 'react-jss';
-import MediaQuery, { useMediaQuery } from 'react-responsive';
+import MediaQuery from 'react-responsive';
 
 import Header from './components/Header';
 import HeaderMobile from './components/HeaderMobile';
@@ -62,176 +62,176 @@ const useStyles = createUseStyles({
     }
 });
 
-const useMobileStylesPortrait = createUseStyles({
-    app: {
-        color: '#FFFFF7',
-        width: '100vw',
-        height: '100vh',      
-        backgroundImage: `url(${MeatImg1})`, 
-        backgroundSize: 'fill', 
-        backgroundRepeat: 'no-repeat'
-    },
-    header: {
-        color: 'black',
-        display: 'flex',
-        flexDirection: 'row-reverse',
-        position: 'fixed',
-        top: 0,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        width: '100%',
-        backgroundColor: '#D7DEE6',
-        height: 40,
-        fontSize: 'calc(10px + 2vmin)',
-    },
-    headerItem: {
-        userSelect: 'none',
-        height: 38,
-        maxWidth: '60%',
-        transition: '.5s',
-        '&:hover': {
-            transform: 'scale(1.05)'
-        }
-    },
-    about: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%'
-    },
-    aboutPanel: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#282c348a',
-        border: '2px solid darkslategray',
-        borderRadius: '8px',
-        margin: 8,
-        padding: 4,
-        width: '70%',
-        overflow: 'scrollY'
-    },
-    menu: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: '90%'
-    },
-    menuButtonContainer: {
-        display: 'flex',
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        width: '100%'
-    },
-    menuButton: {
-        display: 'flex',
-        color: '#000000',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'whitesmoke',
-        border: '3px solid whitesmoke',
-        borderRadius: '8px',
-        userSelect: 'none',
-        cursor: 'pointer',
-        margin: '2px',
-        width: '3rem',
-        height: '30px'
-    }
-});
+// const useMobileStylesPortrait = createUseStyles({
+//     app: {
+//         color: '#FFFFF7',
+//         width: '100vw',
+//         height: '100vh',      
+//         backgroundImage: `url(${MeatImg1})`, 
+//         backgroundSize: 'fill', 
+//         backgroundRepeat: 'no-repeat'
+//     },
+//     header: {
+//         color: 'black',
+//         display: 'flex',
+//         flexDirection: 'row-reverse',
+//         position: 'fixed',
+//         top: 0,
+//         alignItems: 'center',
+//         justifyContent: 'space-around',
+//         width: '100%',
+//         backgroundColor: '#D7DEE6',
+//         height: 40,
+//         fontSize: 'calc(10px + 2vmin)',
+//     },
+//     headerItem: {
+//         userSelect: 'none',
+//         height: 38,
+//         maxWidth: '60%',
+//         transition: '.5s',
+//         '&:hover': {
+//             transform: 'scale(1.05)'
+//         }
+//     },
+//     about: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         width: '100%'
+//     },
+//     aboutPanel: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#282c348a',
+//         border: '2px solid darkslategray',
+//         borderRadius: '8px',
+//         margin: 8,
+//         padding: 4,
+//         width: '70%',
+//         overflow: 'scrollY'
+//     },
+//     menu: {
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         flexDirection: 'column',
+//         height: '90%'
+//     },
+//     menuButtonContainer: {
+//         display: 'flex',
+//         flexDirection: "row",
+//         alignItems: 'center',
+//         justifyContent: 'space-evenly',
+//         width: '100%'
+//     },
+//     menuButton: {
+//         display: 'flex',
+//         color: '#000000',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: 'whitesmoke',
+//         border: '3px solid whitesmoke',
+//         borderRadius: '8px',
+//         userSelect: 'none',
+//         cursor: 'pointer',
+//         margin: '2px',
+//         width: '3rem',
+//         height: '30px'
+//     }
+// });
 
-const useMobileStylesLandscape = createUseStyles({
-    app: {
-        color: '#FFFFF7',
-        width: '100vw',
-        height: '100vh',      
-        backgroundImage: `url(${MeatImg1})`, 
-        backgroundSize: 'fill', 
-        backgroundRepeat: 'no-repeat'
-    },
-    body: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '95%',
-        paddingTop: 20,
-        margin: 0
-    },
-    header: {
-        color: 'black',
-        display: 'flex',
-        flexDirection: 'row-reverse',
-        position: 'fixed',
-        top: 0,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        width: '100%',
-        backgroundColor: '#D7DEE6',
-        height: 40,
-        fontSize: 'calc(10px + 2vmin)',
-    },
-    headerItem: {
-        userSelect: 'none',
-        height: 38,
-        maxWidth: '60%',
-        transition: '.5s',
-        '&:hover': {
-            transform: 'scale(1.05)'
-        }
-    },
-    about: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%'
-    },
-    aboutPanel: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#282c348a',
-        border: '2px solid darkslategray',
-        borderRadius: '8px',
-        margin: 8,
-        padding: 4,
-        width: '70%',
-        overflow: 'scrollY'
-    },
-    menu: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: '90%'
-    },
-    menuButtonContainer: {
-        display: 'flex',
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        width: '100%'
-    },
-    menuButton: {
-        display: 'flex',
-        color: '#000000',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'whitesmoke',
-        border: '3px solid whitesmoke',
-        borderRadius: '8px',
-        userSelect: 'none',
-        cursor: 'pointer',
-        margin: '2px',
-        width: '3rem',
-        height: '30px'
-    }
-});
+// const useMobileStylesLandscape = createUseStyles({
+//     app: {
+//         color: '#FFFFF7',
+//         width: '100vw',
+//         height: '100vh',      
+//         backgroundImage: `url(${MeatImg1})`, 
+//         backgroundSize: 'fill', 
+//         backgroundRepeat: 'no-repeat'
+//     },
+//     body: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         height: '95%',
+//         paddingTop: 20,
+//         margin: 0
+//     },
+//     header: {
+//         color: 'black',
+//         display: 'flex',
+//         flexDirection: 'row-reverse',
+//         position: 'fixed',
+//         top: 0,
+//         alignItems: 'center',
+//         justifyContent: 'space-around',
+//         width: '100%',
+//         backgroundColor: '#D7DEE6',
+//         height: 40,
+//         fontSize: 'calc(10px + 2vmin)',
+//     },
+//     headerItem: {
+//         userSelect: 'none',
+//         height: 38,
+//         maxWidth: '60%',
+//         transition: '.5s',
+//         '&:hover': {
+//             transform: 'scale(1.05)'
+//         }
+//     },
+//     about: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         width: '100%'
+//     },
+//     aboutPanel: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#282c348a',
+//         border: '2px solid darkslategray',
+//         borderRadius: '8px',
+//         margin: 8,
+//         padding: 4,
+//         width: '70%',
+//         overflow: 'scrollY'
+//     },
+//     menu: {
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         flexDirection: 'column',
+//         height: '90%'
+//     },
+//     menuButtonContainer: {
+//         display: 'flex',
+//         flexDirection: "row",
+//         alignItems: 'center',
+//         justifyContent: 'space-evenly',
+//         width: '100%'
+//     },
+//     menuButton: {
+//         display: 'flex',
+//         color: '#000000',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: 'whitesmoke',
+//         border: '3px solid whitesmoke',
+//         borderRadius: '8px',
+//         userSelect: 'none',
+//         cursor: 'pointer',
+//         margin: '2px',
+//         width: '3rem',
+//         height: '30px'
+//     }
+// });
 
 function App() {
     const [isShowMenu, setIsShowMenu] = useState(false);
--- src/components/MenuSectionsMobile.jsx
+++ src/components/MenuSectionsMobile.jsx
@@ -40,17 +40,60 @@ const useStylesLandscape = createUseStyles({
     },
     theMenu: {
         margin: '1rem',
-        height: 300,
-        width: 300
+        height: '75vh',
+        width: '75vw'
+    }
+})
+const useStylesPortrait = createUseStyles({
+    menu: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        marginTop: 30
+    },
+    menuButtonContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%',
+        height: '100%'
+    },
+    menuButton: {
+        color: '#000000',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'whitesmoke',
+        border: '1px solid black',
+        borderRadius: '8px',
+        userSelect: 'none',
+        padding: 4,
+        width: '90%',
+        height: '1rem',
+        whiteSpace: 'normal',
+        textOverflow: '',
+        '&:hover': {
+            transform: ' scale(1.2)',
+        },
+    },
+    theMenu: {
+        margin: '1rem',
+        height: '80vh',
+        width: '80vw'
     }
 })
 
 const MenuSections = () => {
-    const styles = useStylesLandscape();
+    const stylesLandscape = useStylesLandscape();
+    const stylesPortrait = useStylesPortrait();
+
     const [sections, setSections] = useState([
+        'http://placekitten.com/1200/1200',
         'http://placekitten.com/800/800',
-        'http://placekitten.com/800/800',
-        'http://placekitten.com/800/800'
+        'http://placekitten.com/600/600'
     ])
 
     const [sectionToShow, setSectionToShow] = useState(sections[0]);
@@ -62,27 +105,25 @@ const MenuSections = () => {
     return (
         <div>
             <MediaQuery query='(orientation: portrait)'>
-            <div className={styles.menu}>
-                    <img className={styles.theMenu} src={sectionToShow} style={{height: "80%", width: "80%", borderRadius:"16px"}}/>
-                    <div className={styles.menuButtonContainer}>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(0)} >Platters</div>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(1)} >A La Carte</div>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(2)} >Sides</div>
+            <div className={stylesPortrait.menu}>
+                    <img className={stylesPortrait.theMenu} src={sectionToShow} />
+                    <div className={stylesPortrait.menuButtonContainer}>
+                        <div className={stylesPortrait.menuButton} onClick={()=>handleChangePage(0)} >Platters</div>
+                        <div className={stylesPortrait.menuButton} onClick={()=>handleChangePage(1)} >A La Carte</div>
+                        <div className={stylesPortrait.menuButton} onClick={()=>handleChangePage(2)} >Sides</div>
                     </div>
                 </div>
-                <div>You are portrait</div>
             </MediaQuery>
 
             <MediaQuery query='(orientation: landscape)'>
-                <div className={styles.menu}>
-                    <img className={styles.theMenu} src={sectionToShow} style={{height: "80%", width: "80%", borderRadius:"16px"}}/>
-                    <div className={styles.menuButtonContainer}>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(0)} >Platters</div>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(1)} >A La Carte</div>
-                        <div className={styles.menuButton} onClick={()=>handleChangePage(2)} >Sides</div>
+                <div className={stylesLandscape.menu}>
+                    <img className={stylesLandscape.theMenu} src={sectionToShow} />
+                    <div className={stylesLandscape.menuButtonContainer}>
+                        <div className={stylesLandscape.menuButton} onClick={()=>handleChangePage(0)} >Platters</div>
+                        <div className={stylesLandscape.menuButton} onClick={()=>handleChangePage(1)} >A La Carte</div>
+                        <div className={stylesLandscape.menuButton} onClick={()=>handleChangePage(2)} >Sides</div>
                     </div>
                 </div>
-                <div>You are landscape</div>
                 </MediaQuery>
         </div>
     )
