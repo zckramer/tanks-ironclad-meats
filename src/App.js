6b0a97c3a2621ab44166ff84dfb30a468a159232
Zachary Kramer <zckramer83@gmail.com>
2021-02-27 12:52:44 -0500

    routes in place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { 
+    useState, 
+    // useEffect 
+} from 'react';
 import {
+    BrowserRouter as Router,
     Switch,
     Route,
-    useLocation
+    Redirect,
+    // useLocation
 } from 'react-router-dom';
 
 // Do NOT use 'Mobile' named components. Media queries are done in CSS now.
@@ -15,71 +20,73 @@ import Modal from './components/Modal/Modal';
 function App() {
     // const [isShowMenu, setIsShowMenu] = useState(false);
     const [showModal, setShowModal] = useState(false);
-    const [currentURL, setCurrentURL] = useState(useLocation())
+    // const [currentURL, setCurrentURL] = useState(useLocation())
     
-    let location = useLocation();
+    // let location = useLocation();
 
     function handleNavClick (e) {
         console.log(e);
-        console.log(currentURL);
-        setCurrentURL(location);
+        // console.log(currentURL);
+        // setCurrentURL(location);
     }
 
     function handleToggleModal () {
         setShowModal(!showModal);
     }
 
-    function useURLChange() {
-        let location = useLocation();
-        useEffect(() => {
-          setCurrentURL(location);
-        }, [location]);
-    }
-    useURLChange();
+    // function useURLChange() {
+    //     let location = useLocation();
+    //     useEffect(() => {
+    //       setCurrentURL(location);
+    //     }, [location]);
+    // }
+    // useURLChange();
 
     return (
-        <div className='App'>
-            <div className='Body'>
-                <div className='HeaderBlock' />
-                <Header 
-                    navClick={(e)=>handleNavClick(e)}
-                    onClick={(e)=>handleToggleModal(e)}
-                    showModal={showModal}
-                />
-                {showModal ? 
-                    <Modal 
+        <Router>
+            <div className='App'>
+                <div className='Body'>
+                    <div className='HeaderBlock' />
+                    <Header 
+                        navClick={(e)=>handleNavClick(e)}
+                        onClick={(e)=>handleToggleModal(e)}
+                        showModal={showModal}
+                        />
+                    {showModal ? 
+                        <Modal 
                         navClick={(e)=>handleNavClick(e)}
                         about={<About />}
                         menu={<Menu />}
                         merch={<About />}
                         contact={<About />}
-                    /> : null
-                }
-                    <Switch>
-                        <Route path='/'><About /></Route>
-                        <Route path='/about'><About /></Route>
-                        <Route path='/menu'><Menu /></Route>
-                        <Route path='/merch'><About /></Route>
-                        <Route path='/contact'><About /></Route>
-                    </Switch>
-                <p className='Footer' >Web Design/Development by  
-                    <a href='https://www.github.com/zckramer' 
-                        rel='noreferrer'
-                        target='_blank'
-                        style={{
-                            textDecoration:'none', 
-                            color:'inherit', 
-                            backgroundColor:'#282c34DD', 
-                            margin: '4px', 
-                            borderRadius:'6px', 
-                            fontWeight:'bold',
-                        }}
-                        > Zack Kramer 
-                    </a>
-                </p>
+                        /> : null
+                    }
+                        <Switch>
+                            <Route path='/about'><About /></Route>
+                            <Route path='/menu'><Menu /></Route>
+                            <Route path='/merch'><About /></Route>
+                            <Route path='/contact'><About /></Route>
+                            <Redirect to='/about' />
+                        </Switch>
+                    <p className='Footer' >Web Design/Development by  
+                        <a href='https://www.github.com/zckramer' 
+                            rel='noreferrer'
+                            target='_blank'
+                            style={{
+                                textDecoration:'none', 
+                                color:'inherit', 
+                                backgroundColor:'#282c34DD', 
+                                margin: '4px', 
+                                borderRadius:'6px', 
+                                fontWeight:'bold',
+                            }}
+                            > Zack Kramer 
+                        </a>
+                    </p>
+                </div>
+                <div className='FooterBlock' />
             </div>
-            <div className='FooterBlock' />
-        </div>
+        </Router>
     );
 }
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
 const Modal = (props) => {
     return (
                 <div className='ModalMenu' >
-                    <Link to='/about'><div className='ModalMenu__Item' onClick={()=>props.navClick("ABOUT")}>About Tank's</div></Link> 
-                    <Link to='/menu'><div className='ModalMenu__Item' onClick={()=>props.navClick("MENU")}>The Menu</div></Link> 
-                    <Link to='/merch'><div className='ModalMenu__Item' onClick={()=>props.navClick("MERCH")}>Merchandise</div></Link> 
-                    <Link to='/contact'><div className='ModalMenu__Item' onClick={()=>props.navClick("CONTACT")}>Contact Info</div></Link> 
+                    <NavLink to='/about'><div className='ModalMenu__Item' onClick={()=>props.navClick("ABOUT")}>About Tank's</div></NavLink> 
+                    <NavLink to='/menu'><div className='ModalMenu__Item' onClick={()=>props.navClick("MENU")}>The Menu</div></NavLink> 
+                    <NavLink to='/merch'><div className='ModalMenu__Item' onClick={()=>props.navClick("MERCH")}>Merchandise</div></NavLink> 
+                    <NavLink to='/contact'><div className='ModalMenu__Item' onClick={()=>props.navClick("CONTACT")}>Contact Info</div></NavLink> 
                 </div>
     )
 }
