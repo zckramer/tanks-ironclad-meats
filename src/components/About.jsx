f423dc94515be94105542aeca9b275270f174c6a
Zachary Kramer <zckramer83@gmail.com>
2021-03-07 18:56:43 -0500

    changed about info, styling a bit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,10 +13,21 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className='About__Panel'>
+            
+            <div className='About__Panel-Section'>
                 <div className='About__Panel-Header'>
                     Where you can find us: 
                 </div>
+                <div>
+                    <Link 
+                        url='https://www.facebook.com/pages/category/Bar/Brewstirs-Beechwold-Tavern-112882397218391/'
+                        text='Brewstirs Beechwold Tavern'
+                    />
+                </div>
+                <div>Every day, Noon - 11PM</div>
+                <div>4955 Arbor Village Drive, Columbus, Ohio 43215</div>
+            </div>
+            <div className='About__Panel'>
                 <div className='About__Panel-Section'>
                     <div>
                         <Link 
@@ -26,26 +37,6 @@ const About = () => {
                     </div>
                     <div>Every Sunday, 4:30PM - 12AM</div>
                     <div>361 E Whittier St, Columbus, OH 43206</div>
-                </div>
-                <div className='About__Panel-Section'>
-                    <div>
-                        <Link 
-                            url='https://www.facebook.com/pages/category/Bar/Brewstirs-Beechwold-Tavern-112882397218391/'
-                            text='Brewstirs Beechwold Tavern'
-                        />
-                    </div>
-                    <div>Every day, Noon - 11PM</div>
-                    <div>4955 Arbor Village Drive, Columbus, Ohio 43215</div>
-                </div>
-                <div className='About__Panel-Section'>
-                    <div>
-                        <Link
-                            url='https://www.facebook.com/TheZigHaus'
-                            text='Leipzig Haus'
-                        />
-                    </div>
-                    <div>Thursday - Saturday, 5PM - 12AM</div>
-                    <div>2201 East Livingston Ave, Columbus, Ohio 43209</div>
                 </div>
             </div>
         </div>
