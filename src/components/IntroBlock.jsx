f97834c5573aa443c5a1cdfc691eaa526c759ce2
Zachary Kramer <zckramer83@gmail.com>
2021-04-20 18:57:48 -0400

    added video to intro block, cant see on mobile

--- src/components/IntroBlock.jsx
+++ src/components/IntroBlock.jsx
@@ -1,4 +1,5 @@
 import TankLogo from '../images/tank_logo.png';
+import IntroVideo from '../images/RoughDraft01.mp4';
 import meat1 from '../images/meat01.jpg';
 import meat2 from '../images/meat02.jpg';
 import meat3 from '../images/meat03.jpg';
@@ -13,7 +14,14 @@ const IntroBlock = () => {
     return (
         <div className='IntroBlock'>
             <div className='Intro__Logo-Background'>
-                <img className='Intro__Background' alt='Action scenes of meats being cooked' src={imagesArray[1]} />
+                {/* <img className='Intro__Background' alt='Action scenes of meats being cooked' src={imagesArray[1]} /> */}
+                <video autoPlay muted 
+                    className='Intro__Background' 
+                    alt='Action scenes of meats being cooked'
+                    src={IntroVideo} type='video/mp4'
+                >
+                    {/* <source src={IntroVideo} type='video/mp4' /> */}
+                </video>
                 <img className='Intro__Logo' src={TankLogo} alt={`Tank's Ironclad Meats Logo`} />
             </div>
         </div>
